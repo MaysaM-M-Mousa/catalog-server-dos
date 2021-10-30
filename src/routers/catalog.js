29eb12d92a3484a7deb12bcbb1821346f277f691
MaysaM-M-Mousa <maysam.m.mousa@gmail.com>
2021-10-31 00:04:46 +0300

    adding update operation

--- src/routers/catalog.js
+++ src/routers/catalog.js
@@ -8,7 +8,7 @@ router.get('/books/search/:topic', async (req, res) => {
     try {
         const books = await Catalog.find({ topic: req.params.topic })
         if (!books.length) {
-            
+
             return res.status(404).send()
         }
         res.status(200).send(books)
@@ -30,4 +30,28 @@ router.get('/books/info/:name', async (req, res) => {
     }
 })
 
+router.patch('/books/:name', async (req, res) => {
+
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['count', 'price']
+
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+    if (!isValidOperation) {
+        return res.status(500).send({ msg: "unvalid update operation" })
+    }
+    try {
+        const book = await Catalog.findOne({ name: req.params.name })
+        if (!book) {
+            return res.status(404).send()
+        }
+
+        updates.forEach((update) => { book[update] = req.body[update] })
+
+        await book.save()
+        res.status(200).send()
+    } catch (e) {
+        res.status(500).send()
+    }
+
+})
 module.exports = router
